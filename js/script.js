1ba72272fb0d1441be305648dcd7e8a19456cfd1
riderjensen <riderjensen@gmail.com>
2021-09-14 19:07:03 -0600

    init

---
+++ js/script.js
@@ -0,0 +1,57 @@
+(function ($) {
+  'use strict';
+
+  // ----------------------------
+  // AOS
+  // ----------------------------
+  AOS.init({
+    once: true
+  });
+
+  
+  $(window).on('scroll', function () {
+		//.Scroll to top show/hide
+    var scrollToTop = $('.scroll-top-to'),
+      scroll = $(window).scrollTop();
+    if (scroll >= 200) {
+      scrollToTop.fadeIn(200);
+    } else {
+      scrollToTop.fadeOut(100);
+    }
+  });
+
+  $(document).ready(function() {
+
+    // navbarDropdown
+    if ($(window).width() < 992) {
+      $('.main-nav .dropdown-toggle').on('click', function () {
+        $(this).siblings('.dropdown-menu').animate({
+          height: 'toggle'
+        }, 300);
+      });
+    }
+
+    // -----------------------------
+    //  Testimonial Slider
+    // -----------------------------
+    $('.testimonial-slider').slick({
+      slidesToShow: 2,
+      infinite: true,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+
+  });
+
+})(jQuery);
